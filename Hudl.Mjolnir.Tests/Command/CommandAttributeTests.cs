bbd054cf820332b64d501309f5fe1c19c34dcfb8
Rob Hruska <robhruska@gmail.com>
2014-02-07 01:03:43 -0600

    Introduce CommandAttribute and CommandInterceptor

---
+++ Hudl.Mjolnir.Tests/Command/CommandAttributeTests.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Castle.DynamicProxy;
+using Hudl.Mjolnir.Command;
+using Hudl.Mjolnir.Tests.Helper;
+using Xunit;
+
+namespace Hudl.Mjolnir.Tests.Command
+{
+    public class CommandAttributeTests : TestFixture
+    {
+        [Fact]
+        public void String_WithImplementation()
+        {
+            var instance = new TestImplementation();
+            var proxy = CreateForImplementation(instance);
+            
+            var result = proxy.InvokeString();
+
+            Assert.True(instance.IsCompleted);
+            Assert.Equal("Non-task string", result);
+        }
+
+        [Fact]
+        public async Task GenericTask_WithImplementation()
+        {
+            var instance = new TestImplementation();
+            var proxy = CreateForImplementation(instance);
+
+            var task = proxy.InvokeGenericTask();
+            var result = await task;
+
+            Assert.True(instance.IsCompleted);
+            Assert.Equal("Generic task string", result);
+        }
+
+        [Fact]
+        public async Task GenericTask_WithImplementation_RunAndSleep()
+        {
+            var instance = new TestImplementation();
+            var proxy = CreateForImplementation(instance);
+
+            var task = proxy.InvokeGenericTaskWithRunAndSleep();
+            await task;
+
+            Assert.True(instance.IsCompleted);
+        }
+
+        [Fact]
+        public async Task GenericTask_WithImplementation_AwaitDelay()
+        {
+            var instance = new TestImplementation();
+            var proxy = CreateForImplementation(instance);
+
+            var task = proxy.InvokeGenericTaskWithAwaitDelay();
+            await task;
+
+            Assert.True(instance.IsCompleted);
+        }
+
+        [Fact]
+        public async Task UntypedTask_WithImplementation()
+        {
+            var instance = new TestImplementation();
+            var proxy = CreateForImplementation(instance);
+
+            var task = proxy.InvokeUntypedTask();
+
+            Assert.True(task != null);
+            Assert.True(task is Task);
+            Assert.False(task.GetType().IsGenericType);
+
+            await task;
+
+            Assert.True(instance.IsCompleted);
+        }
+
+        [Fact]
+        public void Void_WithImplementation()
+        {
+            var instance = new TestImplementation();
+            var proxy = CreateForImplementation(instance);
+
+            proxy.InvokeVoid();
+
+            Assert.True(instance.IsCompleted);
+        }
+
+        [Fact]
+        public void NoImplementation_Throws()
+        {
+            var proxy = CreateForNoImplementation();
+
+            var ex = Assert.Throws<InvalidOperationException>(() =>
+            {
+                proxy.InvokeGenericTask();
+            });
+
+            Assert.Equal("Invocation target required", ex.Message);
+        }
+
+        [Fact]
+        public void InterfaceMissingAttribute_Throws()
+        {
+            var instance = new TestImplementation();
+            var proxy = CreateForNoAttribute(instance);
+
+            var ex = Assert.Throws<TargetInvocationException>(() =>
+            {
+                proxy.InvokeGenericTask();
+            });
+
+            Assert.Equal("Interface does not have [CommandAttribute]", ex.InnerException.Message);
+        }
+
+        [Fact]
+        public void TimeoutPresentOnInterfaceMethod_OverridesDefaultTimeout()
+        {
+            // TODO
+        }
+
+        [Fact]
+        public void TimeoutPresentOnImplementationMethod_IsIgnored()
+        {
+            // TODO
+        }
+
+        private ITestInterfaceWithImplementation CreateForImplementation(TestImplementation instance)
+        {
+            var generator = new ProxyGenerator();
+            return (ITestInterfaceWithImplementation)generator.CreateInterfaceProxyWithTarget(typeof(ITestInterfaceWithImplementation), instance, new CommandInterceptor());
+        }
+
+        private ITestInterfaceWithoutImplementation CreateForNoImplementation()
+        {
+            var generator = new ProxyGenerator();
+            return (ITestInterfaceWithoutImplementation) generator.CreateInterfaceProxyWithoutTarget(typeof(ITestInterfaceWithoutImplementation), new CommandInterceptor());
+        }
+
+        private ITestInterfaceWithoutAttribute CreateForNoAttribute(TestImplementation instance)
+        {
+            var generator = new ProxyGenerator();
+            return (ITestInterfaceWithoutAttribute)generator.CreateInterfaceProxyWithTarget(typeof(ITestInterfaceWithoutAttribute), instance, new CommandInterceptor());
+        }
+    }
+
+    [Command("foo", "bar", "baz", 10000)]
+    public interface ITestInterfaceWithImplementation
+    {
+        Task<string> InvokeGenericTask();
+        Task<object> InvokeGenericTaskWithRunAndSleep();
+        Task<object> InvokeGenericTaskWithAwaitDelay();
+        Task InvokeUntypedTask();
+        string InvokeString();
+        void InvokeVoid();
+    }
+
+    public class TestImplementation : ITestInterfaceWithImplementation, ITestInterfaceWithoutAttribute
+    {
+        public bool IsCompleted { get; private set; }
+
+        public TestImplementation()
+        {
+            IsCompleted = false;
+        }
+
+        public Task<string> InvokeGenericTask()
+        {
+            IsCompleted = true;
+            return Task.FromResult("Generic task string");
+        }
+
+        public Task<object> InvokeGenericTaskWithRunAndSleep()
+        {
+            return Task.Run(() =>
+            {
+                Thread.Sleep(100);
+                IsCompleted = true;
+                return new object();
+            });
+        }
+
+        public async Task<object> InvokeGenericTaskWithAwaitDelay()
+        {
+            await Task.Delay(100);
+            IsCompleted = true;
+            return new object();
+        }
+
+        public Task InvokeUntypedTask()
+        {
+            return Task.Run(() =>
+            {
+                IsCompleted = true;
+            });
+        }
+
+        public string InvokeString()
+        {
+            IsCompleted = true;
+            return "Non-task string";
+        }
+
+        public void InvokeVoid()
+        {
+            // Nothing.
+            IsCompleted = true;
+        }
+    }
+
+    [Command("foo", "bar", "baz", 10000)]
+    public interface ITestInterfaceWithoutImplementation
+    {
+        Task<string> InvokeGenericTask();
+    }
+
+    public interface ITestInterfaceWithoutAttribute
+    {
+        Task<string> InvokeGenericTask();
+    }
+}
---
+++ Hudl.Mjolnir/Command/CommandAttribute.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Castle.DynamicProxy;
+
+namespace Hudl.Mjolnir.Command
+{
+    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Method)]
+    public sealed class CommandAttribute : Attribute
+    {
+        private const int DefaultTimeout = 15000;
+
+        private readonly string _group;
+        private readonly string _breakerKey;
+        private readonly string _poolKey;
+        private readonly int _timeout;
+
+        public CommandAttribute(string group, string isolationKey, int timeout = DefaultTimeout) : this(group, isolationKey, isolationKey, timeout) {}
+
+        public CommandAttribute(string group, string breakerKey, string poolKey, int timeout = DefaultTimeout)
+        {
+            if (string.IsNullOrWhiteSpace(group))
+            {
+                throw new ArgumentException("group");
+            }
+
+            if (string.IsNullOrWhiteSpace(breakerKey))
+            {
+                throw new ArgumentException("breakerKey");
+            }
+
+            if (string.IsNullOrWhiteSpace(poolKey))
+            {
+                throw new ArgumentNullException("poolKey");
+            }
+
+            if (timeout < 0)
+            {
+                throw new ArgumentException("timeout");
+            }
+
+            _group = group;
+            _breakerKey = breakerKey;
+            _poolKey = poolKey;
+            _timeout = timeout;
+        }
+
+        public string Group
+        {
+            get { return _group; }
+        }
+
+        public string BreakerKey
+        {
+            get { return _breakerKey; }
+        }
+
+        public string PoolKey
+        {
+            get { return _poolKey; }
+        }
+
+        public int Timeout
+        {
+            get { return _timeout; }
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class CommandTimeout : Attribute
+    {
+        private readonly int _timeout;
+
+        public CommandTimeout(int timeout)
+        {
+            if (timeout < 0)
+            {
+                throw new ArgumentException("timeout");
+            }
+
+            _timeout = timeout;
+        }
+
+        public int Timeout
+        {
+            get { return _timeout; }
+        }
+    }
+
+    internal class InvocationCommand<TResult> : Command<TResult>
+    {
+        private readonly IInvocation _invocation;
+
+        public InvocationCommand(string group, string breakerKey, string poolKey, int timeout, IInvocation invocation)
+            : base(group, breakerKey, poolKey, TimeSpan.FromMilliseconds(timeout))
+        {
+            _invocation = invocation;
+        }
+
+        protected override Task<TResult> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            _invocation.Proceed();
+
+            var returnType = _invocation.Method.ReturnType;
+
+            if (typeof (Task).IsAssignableFrom(returnType) && returnType.IsGenericType)
+            {
+                return (Task<TResult>)_invocation.ReturnValue;
+            }
+
+            return Task.FromResult((TResult)_invocation.ReturnValue);
+        }
+    }
+
+    public class CommandInterceptor : IInterceptor
+    {
+        private readonly MethodInfo _invokeCommandAsyncMethod = typeof(CommandInterceptor).GetMethod("InvokeCommandAsync", BindingFlags.NonPublic | BindingFlags.Instance);
+        private readonly MethodInfo _invokeCommandSyncMethod = typeof(CommandInterceptor).GetMethod("InvokeCommandSync", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        public void Intercept(IInvocation invocation)
+        {
+            if (invocation.InvocationTarget == null)
+            {
+                throw new InvalidOperationException("Invocation target required");
+            }
+
+            var returnType = invocation.Method.ReturnType;
+            if (returnType == typeof (void))
+            {
+                // TODO Consider whether this should be a sync or async call. Do callers expect the call to block until the operation is done? Likely.
+                // Maybe we could introduce a [FireAndForget] attribute that would force async and return immediately.
+                _invokeCommandSyncMethod.MakeGenericMethod(typeof (VoidResult)).Invoke(this, new object[] { invocation });
+                return;
+            }
+            
+            if (typeof(Task).IsAssignableFrom(returnType) && returnType.IsGenericType)
+            {
+                var innerType = returnType.GetGenericArguments()[0];
+                invocation.ReturnValue = _invokeCommandAsyncMethod.MakeGenericMethod(innerType).Invoke(this, new object[] { invocation });
+                return;
+            }
+
+            if (typeof(Task).IsAssignableFrom(returnType)) // Non-generic task.
+            {
+                var method = _invokeCommandAsyncMethod.MakeGenericMethod(typeof (VoidResult));
+
+                // This is kind of jank.
+                var task = Task.Factory.StartNew((Action)(() => method.Invoke(this, new object[] { invocation })));
+                invocation.ReturnValue = task;
+                return;
+            }
+
+            invocation.ReturnValue = _invokeCommandSyncMethod.MakeGenericMethod(returnType).Invoke(this, new object[] { invocation });
+        }
+
+        private Task<T> InvokeCommandAsync<T>(IInvocation invocation)
+        {
+            var command = CreateCommand<T>(invocation);
+            return command.InvokeAsync();
+        }
+
+        private T InvokeCommandSync<T>(IInvocation invocation)
+        {
+            var command = CreateCommand<T>(invocation);
+            return command.Invoke();
+        }
+
+        private Command<T> CreateCommand<T>(IInvocation invocation)
+        {
+            var attribute = invocation.Method.DeclaringType.GetCustomAttribute<CommandAttribute>();
+            if (attribute == null)
+            {
+                throw new InvalidOperationException("Interface does not have [CommandAttribute]");
+            }
+
+            var timeoutAttribute = invocation.Method.GetCustomAttribute<CommandTimeout>();
+
+            return new InvocationCommand<T>(
+                attribute.Group,
+                attribute.BreakerKey,
+                attribute.PoolKey,
+                timeoutAttribute != null ? timeoutAttribute.Timeout : attribute.Timeout,
+                invocation);
+        }
+    }
+}
