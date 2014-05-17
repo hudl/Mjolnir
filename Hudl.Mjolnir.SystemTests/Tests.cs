3a0d12e2b455cb2b533c96ca0be697c0d6fbb658
Rob Hruska <robhruska@gmail.com>
2014-05-17 14:36:27 -0500

    HttpServer and example client

---
+++ Hudl.Mjolnir.SystemTests/Tests.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Hudl.Mjolnir.SystemTests
+{
+    public class Tests
+    {
+        private const ushort ServerPort = 22222;
+
+        [Fact]
+        public async Task TestMethod1()
+        {
+            using (var server = new HttpServer(1))
+            {
+                server.Start(ServerPort);
+                server.ProcessRequest += ServerBehavior.Immediate200();
+
+                var client = new HttpClient();
+
+                for(var i = 0; i < 30; i++)
+                {
+                    var response = await client.GetAsync(string.Format("http://localhost:{0}/", ServerPort));
+                    var status = response.StatusCode;
+
+                    Debug.WriteLine("Status {0}", status);
+
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+
+    internal static class ServerBehavior
+    {
+        public static Action<HttpListenerContext> Immediate200()
+        {
+            return context =>
+            {
+                context.Response.StatusCode = (int) HttpStatusCode.OK;
+                context.Response.Close();
+            };
+        }
+
+        public static Action<HttpListenerContext> Delayed200(TimeSpan sleep)
+        {
+            return context =>
+            {
+                Thread.Sleep(sleep);
+                context.Response.StatusCode = (int) HttpStatusCode.OK;
+                context.Response.Close();
+            };
+        }
+
+        public static Action<HttpListenerContext> Percentage500(int percent)
+        {
+            return context =>
+            {
+                var success = (new Random().Next(0, 100)) < percent;
+                context.Response.StatusCode = (int) (success ? HttpStatusCode.OK : HttpStatusCode.InternalServerError);
+                context.Response.Close();
+            };
+        }
+    }
+
+    // From http://stackoverflow.com/a/4673210/29995
+    internal class HttpServer : IDisposable
+    {
+        private readonly HttpListener _listener;
+        private readonly Thread _listenerThread;
+        private readonly Thread[] _workers;
+        private readonly ManualResetEvent _stop, _ready;
+        private Queue<HttpListenerContext> _queue;
+
+        public HttpServer(int maxThreads)
+        {
+            _workers = new Thread[maxThreads];
+            _queue = new Queue<HttpListenerContext>();
+            _stop = new ManualResetEvent(false);
+            _ready = new ManualResetEvent(false);
+            _listener = new HttpListener();
+            _listenerThread = new Thread(HandleRequests);
+        }
+
+        public void Start(int port)
+        {
+            _listener.Prefixes.Add(String.Format(@"http://+:{0}/", port));
+            _listener.Start();
+            _listenerThread.Start();
+
+            for (int i = 0; i < _workers.Length; i++)
+            {
+                _workers[i] = new Thread(Worker);
+                _workers[i].Start();
+            }
+        }
+
+        public void Dispose()
+        {
+            Stop();
+        }
+
+        public void Stop()
+        {
+            _stop.Set();
+            _listenerThread.Join();
+            foreach (Thread worker in _workers)
+                worker.Join();
+            _listener.Stop();
+        }
+
+        private void HandleRequests()
+        {
+            while (_listener.IsListening)
+            {
+                var context = _listener.BeginGetContext(ContextReady, null);
+
+                if (0 == WaitHandle.WaitAny(new[] { _stop, context.AsyncWaitHandle }))
+                    return;
+            }
+        }
+
+        private void ContextReady(IAsyncResult ar)
+        {
+            try
+            {
+                lock (_queue)
+                {
+                    _queue.Enqueue(_listener.EndGetContext(ar));
+                    _ready.Set();
+                }
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void Worker()
+        {
+            WaitHandle[] wait = new[] { _ready, _stop };
+            while (0 == WaitHandle.WaitAny(wait))
+            {
+                HttpListenerContext context;
+                lock (_queue)
+                {
+                    if (_queue.Count > 0)
+                        context = _queue.Dequeue();
+                    else
+                    {
+                        _ready.Reset();
+                        continue;
+                    }
+                }
+
+                try
+                {
+                    ProcessRequest(context);
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine(e);
+                }
+            }
+        }
+
+        public event Action<HttpListenerContext> ProcessRequest;
+    }
+}
