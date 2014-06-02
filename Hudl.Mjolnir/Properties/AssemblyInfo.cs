86bef748b0193cd9c187f83d52590cbbb0e4791b
Rob Hruska <robhruska@gmail.com>
2014-06-01 19:23:24 -0500

    Change version to 2.0.0

--- Hudl.Mjolnir/Properties/AssemblyInfo.cs
+++ Hudl.Mjolnir/Properties/AssemblyInfo.cs
@@ -36,8 +36,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyInformationalVersion("1.8.0.0")]
-[assembly: AssemblyFileVersion("1.8.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
 
 // ONLY change this when the major version changes; never with minor/patch/build versions.
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
