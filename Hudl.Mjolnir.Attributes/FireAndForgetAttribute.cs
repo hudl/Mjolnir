df76aa3dd737e82870743b27f111f5a075ce0dc6
Rob Hruska <robhruska@gmail.com>
2014-07-26 16:05:13 -0500

    Create Hudl.Mjolnir.Attributes package
    This package will replace the command attributes included in Mjolnir
    proper.

---
+++ Hudl.Mjolnir.Attributes/FireAndForgetAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hudl.Mjolnir.Attributes
+{
+    /// <summary>
+    /// Used on methods of <see cref="CommandAttribute"/> interfaces
+    /// to indicate that the call should return immediately and execute
+    /// the method on a background thread.
+    /// 
+    /// Should only be used on interface methods.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class FireAndForgetAttribute : Attribute
+    {
+
+    }
+}
