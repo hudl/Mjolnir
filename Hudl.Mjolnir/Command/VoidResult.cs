b72b2ad165246c6f341cf060be9bfd75b4b483df
Rob Hruska <robhruska@gmail.com>
2014-07-26 16:24:11 -0500

    Deprecate VoidResult

--- Hudl.Mjolnir/Command/VoidResult.cs
+++ Hudl.Mjolnir/Command/VoidResult.cs
@@ -1,5 +1,8 @@
-﻿namespace Hudl.Mjolnir.Command
+﻿using System;
+
+namespace Hudl.Mjolnir.Command
 {
+    [Obsolete("This will likely be removed in a future release.")]
     public class VoidResult
     {
         // TODO rob.hruska 10/18/2013 - Get rid of this void wrapper.
