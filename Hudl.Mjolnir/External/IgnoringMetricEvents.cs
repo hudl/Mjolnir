41bd87c13f1612ab290ba268f185a453ceccd5bf
Rob Hruska <robhruska@gmail.com>
2015-11-23 11:34:38 -0600

    Extract IgnoringMetricEvents into its own file

---
+++ Hudl.Mjolnir/External/IgnoringMetricEvents.cs
@@ -0,0 +1,60 @@
+﻿namespace Hudl.Mjolnir.External
+{
+    internal sealed class IgnoringMetricEvents : IMetricEvents
+    {
+        public void BreakerConfigGauge(string breakerName, long windowMillis, long minimumOps, int thresholdPercent, long tripForMillis)
+        {
+            return;
+        }
+
+        public void BreakerFailureCount(string breakerName, string commandName)
+        {
+            return;
+        }
+
+        public void BreakerFixed(string breakerName)
+        {
+            return;
+        }
+
+        public void BreakerSuccessCount(string breakerName, string commandName)
+        {
+            return;
+        }
+
+        public void BreakerTripped(string breakerName)
+        {
+            return;
+        }
+
+        public void BulkheadConfigGauge(string bulkheadName, string bulkheadType, int maxConcurrent)
+        {
+            return;
+        }
+
+        public void CommandInvoked(string commandName, double invokeMillis, double executeMillis, string status, string failureMode)
+        {
+            return;
+        }
+
+        public void EnterBulkhead(string bulkheadName, string commandName)
+        {
+            return;
+        }
+
+        public void LeaveBulkhead(string bulkheadName, string commandName)
+        {
+            return;
+        }
+
+        public void RejectedByBreaker(string breakerName, string commandName)
+        {
+            return;
+        }
+
+        public void RejectedByBulkhead(string bulkheadName, string commandName)
+        {
+            return;
+        }
+    }
+}
