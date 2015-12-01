8142be0dd0f44d03f9532bb19f056f145ed56d0c
Rob Hruska <robhruska@gmail.com>
2015-12-01 09:02:54 -0600

    Throw exception if value accessed on non-success

--- Hudl.Mjolnir/Command/CommandResult.cs
+++ Hudl.Mjolnir/Command/CommandResult.cs
@@ -13,10 +13,33 @@ namespace Hudl.Mjolnir.Command
         private readonly TResult _value;
         private readonly Exception _exception;
 
-        public TResult Value { get { return _value; } }
-        public Exception Exception { get { return _exception; } }
+        /// <summary>
+        /// Whether or not command execution was successful. If the command failed or Mjolnir
+        /// rejected the command, this will be false.
+        /// </summary>
         public bool WasSuccess { get { return _exception == null; } }
 
+        /// <summary>
+        /// Gets the Value of the command execution. This will throw an exception if accessed
+        /// when WasSuccess == false, so check the WasSuccess property before retrieving.
+        /// </summary>
+        public TResult Value
+        {
+            get
+            {
+                if (!WasSuccess)
+                {
+                    throw new InvalidOperationException("Cannot access CommandResult Value property for unsuccessful result. Check WasSuccess before accessing the Value.");
+                }
+                return _value;
+            }
+        }
+
+        /// <summary>
+        /// If WasSuccess == false, will contain the causing exception.
+        /// </summary>
+        public Exception Exception { get { return _exception; } }
+        
         internal CommandResult(TResult value, Exception exception = null)
         {
             _value = value;
