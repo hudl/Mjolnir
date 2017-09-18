da7556c1ee8e93211df00bd6a8ec48305c84d5c0
Rob Hruska <robhruska@gmail.com>
2017-09-18 13:38:18 -0500

    Func<TResult> as an alternative to Command inheritance

---
+++ Hudl.Mjolnir/Command/DelegateSyncCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace Hudl.Mjolnir.Command
+{
+    internal class DelegateSyncCommand<TResult> : SyncCommand<TResult>
+    {
+        private readonly Func<CancellationToken?, TResult> _func;
+
+        public DelegateSyncCommand(string group, Func<CancellationToken?, TResult> func) : base(group, group, TimeSpan.FromMilliseconds(2000))
+        {
+            _func = func ?? throw new ArgumentNullException(nameof(func));
+        }
+
+        public override TResult Execute(CancellationToken cancellationToken)
+        {
+            return _func(cancellationToken);
+        }
+    }
+}
