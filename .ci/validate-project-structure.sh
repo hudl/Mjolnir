fdc22a5c19b269ca11d8a9aa062d75500507a4ec
Rob Hruska <robhruska@gmail.com>
2023-11-29 14:30:02 -0600

    Get rid of .ci build files

--- .ci/validate-project-structure.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-find . -name "*.csproj" | xargs grep -H "<Version>"
-if [ $? -eq 0 ]; then
-    echo '<Version> tag found in csproj file. Please remove <Version> and replace with <VersionPrefix>.'
-    exit 1
-fi
-
-exit 0
